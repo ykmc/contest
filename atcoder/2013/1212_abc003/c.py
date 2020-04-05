2368c2c71045f708682fe6a5f04752a563694d40
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-05 19:43:41 +0900

    AC: atcoder/2013/1212_abc003/c.py

--- atcoder/2013/1212_abc003/c.py
+++ atcoder/2013/1212_abc003/c.py
@@ -11,3 +11,13 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+R = list(map(int,input().split()))
+
+R.sort(reverse=True)
+
+rate = 0
+for i in range(K):
+    rate = (rate + R[K-1-i])/2
+
+print(rate)
