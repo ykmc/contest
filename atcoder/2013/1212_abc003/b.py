50a6021a41ebe039b94802421aa8c5335b73c46b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-05 19:39:56 +0900

    AC: atcoder/2013/1212_abc003/b.py

--- atcoder/2013/1212_abc003/b.py
+++ atcoder/2013/1212_abc003/b.py
@@ -11,3 +11,26 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+T = input().rstrip()
+
+is_win = True
+
+N = len(S)
+for i in range(N):
+    if S[i]=="@" and T[i]=="@":
+        continue
+    if S[i]=="@":
+        if T[i] not in "atcoder":
+            is_win = False
+            break
+    elif T[i]=="@":
+        if S[i] not in "atcoder":
+            is_win = False
+            break
+    else:
+        if S[i] != T[i]:
+            is_win = False
+            break
+
+print("You can win" if is_win else "You will lose")
