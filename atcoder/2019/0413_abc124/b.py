fdc637aea43275357e978fe1d4dda51081d9ee5e
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-06 19:55:23 +0900

    AC: atcoder/2019/0413_abc124/b.py

--- atcoder/2019/0413_abc124/b.py
+++ atcoder/2019/0413_abc124/b.py
@@ -11,3 +11,15 @@ import numpy as np
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+H = list(map(int,input().split()))
+
+ans = 0
+highest = 0
+
+for h in H:
+    if h >= highest:
+        ans += 1
+        highest = h
+
+print(ans)
