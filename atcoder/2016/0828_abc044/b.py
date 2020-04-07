743799db8cf76786c7b0903cce1a587b5216c65e
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-07 19:58:48 +0900

    AC: atcoder/2016/0828_abc044/b.py

--- atcoder/2016/0828_abc044/b.py
+++ atcoder/2016/0828_abc044/b.py
@@ -1,7 +1,8 @@
 # python 3.4.3
 import sys
 input = sys.stdin.readline
-import numpy as np
+# import numpy as np
+from collections import Counter
 
 # -------------------------------------------------------------
 # function
@@ -11,3 +12,14 @@ import numpy as np
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+W = input().rstrip()
+
+C = Counter(W).most_common()
+
+ans = "Yes"
+for _,cnt in C:
+    if cnt%2 == 1:
+        ans = "No"
+        break
+
+print(ans)
