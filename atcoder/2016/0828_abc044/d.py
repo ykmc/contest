15989aa13796563611c27f34bc0deb7314ba4cf2
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-07 20:18:17 +0900

    AC: atcoder/2016/0828_abc044/d.py

--- atcoder/2016/0828_abc044/d.py
+++ atcoder/2016/0828_abc044/d.py
@@ -1,13 +1,43 @@
 # python 3.4.3
 import sys
 input = sys.stdin.readline
-import numpy as np
 
 # -------------------------------------------------------------
 # function
 # -------------------------------------------------------------
+def f(b,n):
+    if n < b:
+        return n
+    else:
+        return f(b, n//b) + n%b
 
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+S = int(input())
+
+ans = -1
+if S==N:
+    ans = N+1
+else:
+    # bをN^(1/2)まで全探索
+    for b in range(2, int(N**.5)+1):
+        if f(b,N)==S:
+            ans = b
+            break
+
+# まだ答えが見つからなければ
+if ans == -1:
+    # bがN^(1/2)より大きい可能性が残っており、b進数で表すと2桁
+    # 1 <= p < b, N = pb+q <= pb < p^2, p > N^(1/2) なので pで全探索
+    # p+q = S より b = (N-S)/p+1
+    for p in range(int(N**.5)+1,0,-1):
+        b = (N-S)//p + 1
+        # bが1のときは対象外にしないと無限ループ！
+        if b >= 2 and f(b,N)==S:
+            ans = b
+            break
+
+print(ans)
