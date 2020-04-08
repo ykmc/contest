47837c4e2500667ad238e3f11da08e382bdc948b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-08 20:25:44 +0900

    AC: atcoder/2014/0322_abc005/c.py

--- atcoder/2014/0322_abc005/c.py
+++ atcoder/2014/0322_abc005/c.py
@@ -10,3 +10,20 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+T = int(input())
+N = int(input())
+A = list(map(int,input().split()))
+M = int(input())
+B = list(map(int,input().split()))
+
+ans = "yes"
+
+for b in B:
+    for i in range(len(A)):
+        if 0 <= b-A[i] <= T:
+            A.pop(i)
+            break
+    else:
+        ans = "no"
+
+print(ans)
