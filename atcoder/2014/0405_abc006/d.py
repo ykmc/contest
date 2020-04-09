17cf09b624b0bf4862f7ce0a7af485b3c9a78bd7
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-09 21:00:58 +0900

    AC: atcoder/2014/0405_abc006/d.py

--- atcoder/2014/0405_abc006/d.py
+++ atcoder/2014/0405_abc006/d.py
@@ -1,12 +1,31 @@
 # Python3 (3.4.3)
 import sys
 input = sys.stdin.readline
+import bisect
 
 # -------------------------------------------------------------
 # function
 # -------------------------------------------------------------
+class LIS():
+    def __init__(self, a):
+        self.lis = [a[0]]
+        for i in range(len(a)):
+            if a[i] > self.lis[-1]:
+                self.lis.append(a[i])
+            else:
+                self.lis[bisect.bisect_left(self.lis, a[i])] = a[i]
+    
+    def get(self):
+        return self.lis
 
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())g
+C = [int(input()) for _ in range(N)]
+
+L = LIS(C)
+lis = L.get()
+
+print(N-len(lis))
