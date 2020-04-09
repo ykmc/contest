407a963551c2225151ec2648380c3f73f0c3f326
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-09 21:14:20 +0900

    AC: atcoder/2019/0406_abc123/b.py

--- atcoder/2019/0406_abc123/b.py
+++ atcoder/2019/0406_abc123/b.py
@@ -1,6 +1,7 @@
 # Python3 (3.4.3)
 import sys
 input = sys.stdin.readline
+from math import ceil,floor
 
 # -------------------------------------------------------------
 # function
@@ -10,3 +11,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A = [int(input()) for _ in range(5)]
+
+total = 0
+rem = 10
+for a in A:
+    total += ceil(a/10)*10
+    rem = min(rem, (a-1)%10+1)
+
+print(total-10+rem)
