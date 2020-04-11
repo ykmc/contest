e89c43a9851b1c90b53ed35240a490a3c676578e
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-11 21:04:10 +0900

    AC: atcoder/2018/0128_abc087/c.py

--- atcoder/2018/0128_abc087/c.py
+++ atcoder/2018/0128_abc087/c.py
@@ -10,3 +10,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A1 = list(map(int,input().split()))
+A2 = list(map(int,input().split()))
+
+ans = 0
+for i in range(N):
+    ans = max(ans, sum(A1[0:i+1])+sum(A2[i:N]))
+
+print(ans)
