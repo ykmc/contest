dc10ca99eb94ef32e524fb4f982fb3fd5a765e0d
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-11 20:53:25 +0900

    AC: atcoder/2018/0128_abc087/b.py

--- atcoder/2018/0128_abc087/b.py
+++ atcoder/2018/0128_abc087/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())
+
+ans = 0
+for ai in range(A+1):
+    for bi in range(B+1):
+        for ci in range(C+1):
+            if 500*ai + 100*bi + 50*ci == X:
+                ans += 1
+
+print(ans)
