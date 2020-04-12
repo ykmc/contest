13dd7c4dc4171d1918330aceb3ef181f7c7bd17c
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-12 16:31:55 +0900

    AC: atcoder/2016/0911_abc045/d.py

--- atcoder/2016/0911_abc045/d.py
+++ atcoder/2016/0911_abc045/d.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+from collections import defaultdict
+from collections import Counter
+H,W,N = map(int,input().split())
+AB = [list(map(int,input().split())) for _ in range(N)]
+
+# (x,y)を左上とする3x3の正方形に(a,b)が含まれるなら +1
+dic = defaultdict(int)
+for a,b in AB:
+    for dh in range(3):
+        for dw in range(3):
+            if 0 < a-dh <= H-2 and 0 < b-dw <= W-2:
+                dic[(a-dh,b-dw)] += 1
+ 
+Ans = Counter(dic.values())
+Ans[0] = (H-2)*(W-2) - sum(Ans.values())
+ 
+for i in range(10):
+    print(Ans[i])
