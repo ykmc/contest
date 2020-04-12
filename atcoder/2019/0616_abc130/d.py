b6ed5085d3f56822d602c86326960c3173780e6c
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-12 18:18:20 +0900

    AC: atcoder/2019/0616_abc130/d.py

--- atcoder/2019/0616_abc130/d.py
+++ atcoder/2019/0616_abc130/d.py
@@ -10,3 +10,25 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+
+ans = 0
+total = 0
+ri = 0
+for li in range(N):
+    # 右端を更新
+    while ri < N:
+        if total < K:
+            total += A[ri]
+            ri += 1
+        else:
+            break
+    if total < K:
+        break
+    # 数え上げ
+    ans += N-ri+1
+    # 左端を更新
+    total -= A[li]
+
+print(ans)
