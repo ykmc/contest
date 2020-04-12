8093126ef9fd74e6e77d994fb771cfd60b5cc9ea
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-12 16:50:50 +0900

    AC: atcoder/2019/0324_abc122/c.py

--- atcoder/2019/0324_abc122/c.py
+++ atcoder/2019/0324_abc122/c.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,Q = map(int,input().split())
+S = input().rstrip() + "Z"
+LR = [map(int,input().split()) for _ in range(Q)]
+
+A = [0]*(N+1)
+for i in range(N):
+    if S[i]=="A" and S[i+1]=="C":
+        A[i+1] = 1
+for i in range(i):
+    A[i+1] += A[i]
+
+for l,r in LR:
+    print(A[r-1]-A[l-1])
