ce5f7915b2c7f84c083483e94f471f8932c484ca
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 14:42:13 +0900

    AC: atcoder/2019/0309_abc121/b.py

--- atcoder/2019/0309_abc121/b.py
+++ atcoder/2019/0309_abc121/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M,C = map(int,input().split())
+B = list(map(int,input().split()))
+A = [list(map(int,input().split())) for _ in range(N)]
+
+ans = 0
+for i in range(N):
+    x = C
+    for j in range(M):
+        x += A[i][j]*B[j]
+    if x > 0:
+        ans += 1
+
+print(ans)
