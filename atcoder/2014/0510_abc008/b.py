c1d559a63c9dff22c6e95846b6cedfe12f7bf54d
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 09:52:32 +0900

    AC: atcoder/2014/0510_abc008/b.py

--- atcoder/2014/0510_abc008/b.py
+++ atcoder/2014/0510_abc008/b.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+from collections import defaultdict
+d = defaultdict(int)
+
+N = int(input())
+S = [input().rstrip() for _ in range(N)]
+
+for s in S:
+    d[s] += 1
+
+a = sorted(d.items(), key = lambda x:x[1], reverse=True)
+print(a[0][0])
