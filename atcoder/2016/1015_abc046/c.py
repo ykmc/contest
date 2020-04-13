607b1ba1d4c346db0b2d5f927c9729e044f83481
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 14:07:59 +0900

    AC: atcoder/2016/1015_abc046/c.py

--- atcoder/2016/1015_abc046/c.py
+++ atcoder/2016/1015_abc046/c.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+from math import ceil
+from decimal import *
+N = int(input())
+TA = [list(map(int,input().split())) for _ in range(N)]
+ 
+ 
+t,a = 1,1
+for i in range(N):
+    n = Decimal(max(ceil(t/TA[i][0]), ceil(a/TA[i][1])))
+    t = n*TA[i][0]
+    a = n*TA[i][1]
+ 
+print(t+a)
