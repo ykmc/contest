54e23d6f6d6464c32aab1a1d81acefe803a722b0
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 14:08:05 +0900

    AC: atcoder/2016/1015_abc046/d.py

--- atcoder/2016/1015_abc046/d.py
+++ atcoder/2016/1015_abc046/d.py
@@ -10,3 +10,13 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+ 
+ans = 0
+for i in range(len(S)):
+    if i%2==0 and S[i]=="p":
+        ans -= 1
+    elif i%2==1 and S[i]=="g":
+        ans += 1
+ 
+print(ans)
