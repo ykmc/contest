916f45e134862ece47a438d8a4f103c7f1094056
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 14:20:24 +0900

    AC: atcoder/2018/0304_abc089/c.py

--- atcoder/2018/0304_abc089/c.py
+++ atcoder/2018/0304_abc089/c.py
@@ -10,3 +10,20 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+S = [input().rstrip() for _ in range(N)]
+
+from collections import defaultdict
+d = defaultdict(int)
+
+for s in S:
+    key = s[0]
+    d[key] += 1
+
+from itertools import combinations
+c = combinations("MARCH",3)
+ans = 0
+for x,y,z in c:
+    ans += d[x]*d[y]*d[z]
+
+print(ans)
