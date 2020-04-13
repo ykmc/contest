7fc172a37fd691e342d5d51c849ad17867b78277
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-13 14:48:17 +0900

    AC: atcoder/2019/0309_abc121/c.py

--- atcoder/2019/0309_abc121/c.py
+++ atcoder/2019/0309_abc121/c.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+AB = [tuple(map(int,input().split())) for _ in range(N)]
+
+AB.sort()
+
+ans = 0
+cnt = 0
+for a,b in AB:
+    ans += a * min(b, M-cnt)
+    cnt += b
+    if cnt >= M:
+        break
+
+print(ans)
