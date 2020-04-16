aafb390378efe6b7fa30c729e8e486fcb7b3d3ea
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-16 14:16:36 +0900

    AC: atcoder/2018/0311_abc090/d.py

--- atcoder/2018/0311_abc090/d.py
+++ atcoder/2018/0311_abc090/d.py
@@ -10,3 +10,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+
+ans = 0
+for b in range(K+1,N+1):
+    ans += (N//b)*max(0,b-K) + max(0,N%b-K+1)
+    if K==0:
+        ans -= 1
+
+print(ans)
