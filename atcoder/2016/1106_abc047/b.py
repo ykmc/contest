6e2bd5a5edb3ffb6442df6f32f2058ec35ecc37d
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-22 19:34:08 +0900

    AC: atcoder/2016/1106_abc047/b.py

--- atcoder/2016/1106_abc047/b.py
+++ atcoder/2016/1106_abc047/b.py
@@ -10,3 +10,22 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+W,H,N = map(int,input().split())
+XYA = [list(map(int,input().split())) for _ in range(N)]
+
+x_lb,x_ub,y_lb,y_ub = 0,W,0,H
+
+for x,y,a in XYA:
+    if a==1:
+        x_lb = max(x_lb,x)
+    elif a==2:
+        x_ub = min(x_ub,x)
+    elif a==3:
+        y_lb = max(y_lb,y)
+    else:
+        y_ub = min(y_ub,y)
+
+x = max(0, x_ub - x_lb)
+y = max(0, y_ub - y_lb)
+
+print(x*y)
