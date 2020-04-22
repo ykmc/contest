f8f9dee96bb4ad9445972a9eeb14647b9280bdd5
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-22 19:41:00 +0900

    AC: atcoder/2016/1106_abc047/d.py

--- atcoder/2016/1106_abc047/d.py
+++ atcoder/2016/1106_abc047/d.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,T = map(int,input().split())
+A = list(map(int,input().split()))
+ 
+lowest = 10**9+1
+profit = 0
+ans = 0
+
+# 買った価格 - 売った価格 が等しくなる区間がいくつあるか が答え(それらの店全てで1円ずつ値上げすればよいので)
+# 現時点で購入可能な最安値を記録しておき, 移動するたびに利益が更新可能かチェックする
+for a in A:
+    lowest = min(lowest, a)
+    if a - lowest == profit:
+        ans += 1
+    elif a - lowest > profit:
+        profit = a - lowest
+        ans = 1
+ 
+print(ans)
