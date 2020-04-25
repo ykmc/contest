19e8fd8e7d4c60a8f94307716bc5d6da5f3ae3cc
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-25 10:13:16 +0900

    AC: atcoder/2014/0607_abc010/d.py

--- atcoder/2014/0607_abc010/d.py
+++ atcoder/2014/0607_abc010/d.py
@@ -5,8 +5,77 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # function
 # -------------------------------------------------------------
+from collections import deque
+class Dinic:
+    def __init__(self, N):
+        self.N = N
+        self.G = [[] for i in range(N)]
 
+    def add_edge(self, fr, to, cap):
+        forward = [to, cap, None]
+        forward[2] = backward = [fr, 0, forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def add_multi_edge(self, v1, v2, cap1, cap2):
+        edge1 = [v2, cap1, None]
+        edge1[2] = edge2 = [v1, cap2, edge1]
+        self.G[v1].append(edge1)
+        self.G[v2].append(edge2)
+
+    def bfs(self, s, t):
+        self.level = level = [None]*self.N
+        deq = deque([s])
+        level[s] = 0
+        G = self.G
+        while deq:
+            v = deq.popleft()
+            lv = level[v] + 1
+            for w, cap, _ in G[v]:
+                if cap and level[w] is None:
+                    level[w] = lv
+                    deq.append(w)
+        return level[t] is not None
+
+    def dfs(self, v, t, f):
+        if v == t:
+            return f
+        level = self.level
+        for e in self.it[v]:
+            w, cap, rev = e
+            if cap and level[v] < level[w]:
+                d = self.dfs(w, t, min(f, cap))
+                if d:
+                    e[1] -= d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self, s, t):
+        flow = 0
+        INF = 10**9 + 7
+        G = self.G
+        while self.bfs(s, t):
+            *self.it, = map(iter, self.G)
+            f = INF
+            while f:
+                f = self.dfs(s, t, INF)
+                flow += f
+        return flow
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,G,E = map(int,input().split())
+P = list(map(int,input().split()))
+AB = [tuple(map(int,input().split())) for _ in range(E)]
+
+dinic = Dinic(N+1)
+for a,b in AB:
+    dinic.add_edge(a,b,1)
+    dinic.add_edge(b,a,1)
+for p in P:
+    dinic.add_edge(p,N,1)
+
+ans = dinic.flow(0,N)
+print(ans)
