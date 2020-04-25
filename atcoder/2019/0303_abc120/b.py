16a760ede56ffc94b75d60932cd8b235423bdb75
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-25 10:46:41 +0900

    AC: atcoder/2019/0303_abc120/b.py

--- atcoder/2019/0303_abc120/b.py
+++ atcoder/2019/0303_abc120/b.py
@@ -10,3 +10,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A,B,K = map(int,input().split())
+
+ans = []
+for i in range(1,101):
+    if A%i==0 and B%i==0:
+        ans.append(i)
+
+ans.sort(reverse=True)
+print(ans[K-1])
