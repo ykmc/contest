cbfd6ef710bec182482e5f3113103b757ecc3484
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-26 08:54:20 +0900

    AC: atcoder/2019/0622_abc131/d.py

--- atcoder/2019/0622_abc131/d.py
+++ atcoder/2019/0622_abc131/d.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+AB = [tuple(map(int,input().split())) for _ in range(N)]
+
+AB.sort(key=lambda x:x[1]) 
+
+ans = "Yes"
+total = 0
+for a,b in AB:
+    total += a
+    if total > b:
+        ans = "No"
+        break
+
+print(ans)
