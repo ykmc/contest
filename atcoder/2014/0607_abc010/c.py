196e580e6fb7f092c9d2637a4991aa9238e18188
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-25 10:13:12 +0900

    AC: atcoder/2014/0607_abc010/c.py

--- atcoder/2014/0607_abc010/c.py
+++ atcoder/2014/0607_abc010/c.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+TXa,TYa,TXb,TYb,T,V = map(int,input().split())
+N = int(input())
+XY = [tuple(map(int,input().split())) for _ in range(N)]
+
+ans = "NO"
+for x,y in XY:
+    if ((TXa - x)**2 + (TYa - y)**2)**.5 + ((TXb - x)**2 + (TYb - y)**2)**.5 <= T*V:
+        ans = "YES"
+        break
+
+print(ans)
