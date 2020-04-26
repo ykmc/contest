51f3e881f8c4121f7b74b48d622bafdc5affc860
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-27 08:51:12 +0900

    AC: atcoder/2014/0712_abc012/d.py

--- atcoder/2014/0712_abc012/d.py
+++ atcoder/2014/0712_abc012/d.py
@@ -10,3 +10,22 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+ABT = [tuple(map(int,input().split())) for _ in range(M)]
+
+import numpy as np
+import scipy.sparse.csgraph as csg
+inf = float("inf")
+
+A = np.array([[inf]*N]*N)
+for a,b,t in ABT:
+    A[a-1][b-1] = t
+    A[b-1][a-1] = t
+
+A = csg.floyd_warshall(A)
+ 
+ans = inf
+for i in range(N):
+    ans = min(ans, max(A[i]))
+ 
+print(int(ans))
