18f8ddd2e4a532a7c2da3740cd17b285a1ef0ce1
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-26 10:42:40 +0900

    AC: atcoder/2019/0622_abc131/e.py

--- atcoder/2019/0622_abc131/e.py
+++ atcoder/2019/0622_abc131/e.py
@@ -10,3 +10,23 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+
+Ans = []
+for i in range(N-1):
+    Ans.append((N-1,i))
+
+cnt = (N-1)*(N-2)//2
+if cnt < K:
+    print(-1)
+    sys.exit()
+
+for i in range(N-1):
+    for j in range(i+1,N-1):
+        if cnt>K:
+            Ans.append((i,j))
+            cnt -= 1
+
+print(len(Ans))
+for u,v in Ans:
+    print(u+1,v+1)
