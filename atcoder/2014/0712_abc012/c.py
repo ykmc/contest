62c45693c9d1c787f5c95af6cc48c1fac43972bc
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-27 08:51:05 +0900

    AC: atcoder/2014/0712_abc012/c.py

--- atcoder/2014/0712_abc012/c.py
+++ atcoder/2014/0712_abc012/c.py
@@ -10,3 +10,19 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+
+total = 0
+for i in range(1,10):
+    for j in range(1,10):
+        total += i*j
+
+M = total - N
+Ans = []
+for i in range(1,M+1):
+    j = M//i
+    if i*j==M and 1<=i<=9 and 1<=j<=9:
+        Ans.append((i,j))
+
+for i,j in Ans:
+    print(i, "x", j)
