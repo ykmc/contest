422664c683cf474a6dba96ce6b4bc48bf2b630e4
ykmc <34961813+ykmc@users.noreply.github.com>
2020-04-26 20:49:24 +0900

    AC: atcoder/2014/0621_abc011/c.py

--- atcoder/2014/0621_abc011/c.py
+++ atcoder/2014/0621_abc011/c.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+NG = [int(input()) for _ in range(3)]
+
+inf = float("inf")
+dp = [inf]*(N+4)
+dp[N] = 0
+for i in range(N):
+    dp[N-1-i] = min(
+        dp[N-1-i+1]+1 if N-1-i+1 not in NG else inf, \
+        dp[N-1-i+2]+1 if N-1-i+2 not in NG else inf, \
+        dp[N-1-i+3]+1 if N-1-i+3 not in NG else inf, \
+    )
+
+print("YES" if dp[0]<=100 else "NO")
