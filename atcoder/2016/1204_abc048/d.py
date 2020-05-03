9be5d2792fe22131c5b7b85ec3345e117b31003a
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-03 14:13:23 +0900

    AC: atcoder/2016/1204_abc048/d.py

--- atcoder/2016/1204_abc048/d.py
+++ atcoder/2016/1204_abc048/d.py
@@ -10,3 +10,9 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+
+if S[0]==S[-1]:
+    print("Second" if len(S)%2==1 else "First")
+else:
+    print("First" if len(S)%2==1 else "Second")
