9bcb2cacee34eb7ad7fe74a93b2b709fb8fb5dff
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-03 14:13:17 +0900

    AC: atcoder/2016/1204_abc048/c.py

--- atcoder/2016/1204_abc048/c.py
+++ atcoder/2016/1204_abc048/c.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,X = map(int,input().split())
+A = list(map(int,input().split()))
+
+ans = 0 
+if A[0]>X:
+    ans = A[0]-X
+    A[0] -= ans
+
+for i in range(N-1):
+    r = max(A[i]+A[i+1]-X,0)
+    A[i+1] -= r
+    ans += r
+
+print(ans)
