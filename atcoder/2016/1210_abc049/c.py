b0489f2f7e0aeb325b93784b9223aed68e428bbc
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-06 21:20:32 +0900

    AC: atcoder/2016/1210_abc049/c.py

--- atcoder/2016/1210_abc049/c.py
+++ atcoder/2016/1210_abc049/c.py
@@ -11,3 +11,22 @@ import numpy as np
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+S = S[::-1]
+
+words = ["dream","dreamer","erase","eraser"]
+for i in range(4):
+    words[i] = words[i][::-1]
+ 
+i = 0
+while i < len(S):
+    for w in words:
+        l = len(w)
+        if w == S[i:i+l]:
+            i += l
+            break
+    else:
+        print("NO")
+        sys.exit()
+ 
+print("YES")
