e3699939bd027d301eb1644685ec4b5d5021dea7
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-06 13:20:55 +0900

    AC: atcoder/2019/0224_abc119/b.py

--- atcoder/2019/0224_abc119/b.py
+++ atcoder/2019/0224_abc119/b.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+XU = [tuple(input().split()) for _ in range(N)]
+
+ans = 0
+for x,u in XU:
+    if u == "JPY":
+        ans += float(x)
+    else:
+        ans += float(x)*380000
+
+print(ans)
