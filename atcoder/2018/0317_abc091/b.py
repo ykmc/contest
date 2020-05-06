59bdcb46325dfe6d9bf63ba835d9e27d6c3af7b6
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-06 11:08:57 +0900

    AC: atcoder/2018/0317_abc091/b.py

--- atcoder/2018/0317_abc091/b.py
+++ atcoder/2018/0317_abc091/b.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+S = [input().rstrip() for _ in range(N)]
+M = int(input())
+T = [input().rstrip() for _ in range(M)]
+
+from collections import defaultdict
+dic = defaultdict(int)
+
+for s in S:
+    dic[s] += 1
+for t in T:
+    dic[t] -= 1
+
+ans = 0
+for v in dic.values():
+    ans = max(ans, v)
+
+print(ans)
