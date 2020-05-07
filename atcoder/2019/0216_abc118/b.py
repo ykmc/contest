0cd6f25c94a43128b78381fecedc8e0ee4f71ca6
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-07 21:15:31 +0900

    AC: atcoder/2019/0216_abc118/b.py

--- atcoder/2019/0216_abc118/b.py
+++ atcoder/2019/0216_abc118/b.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+AA = []
+for _ in range(N):
+    KA = list(map(int,input().split()))
+    AA.append(KA[1::])
+
+from collections import defaultdict
+dic = defaultdict(int)
+for A in AA:
+    for a in A:
+        dic[a] += 1
+
+ans = 0
+for v in dic.values():
+    if v==N:
+        ans += 1
+
+print(ans)
