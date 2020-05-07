9c0e9986b2650a767360d20430cbfcf11d04ad3c
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-07 21:54:24 +0900

    AC: atcoder/2019/0707_abc133/b.py

--- atcoder/2019/0707_abc133/b.py
+++ atcoder/2019/0707_abc133/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,D = map(int,input().split())
+X = [list(map(int,input().split())) for _ in range(N)]
+
+ans = 0
+for i in range(N):
+    for j in range(i+1,N):
+        total = 0
+        for k in range(D):
+            total += (X[i][k] - X[j][k])**2
+        if int(total**.5)**2 == total:
+            ans += 1
+
+print(ans)
