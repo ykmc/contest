1c67423531dd2ec32ab4ca0a429c3dc5bfd46d4e
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-07 21:59:26 +0900

    AC: atcoder/2019/0707_abc133/c.py

--- atcoder/2019/0707_abc133/c.py
+++ atcoder/2019/0707_abc133/c.py
@@ -10,3 +10,15 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+L,R = map(int,input().split())
+
+ans = 2019
+for i in range(L,R+1):
+    if ans==0:
+        break
+    for j in range(i+1,R+1):
+        ans = min(ans, (i*j)%2019)
+        if ans==0:
+            break
+
+print(ans)
