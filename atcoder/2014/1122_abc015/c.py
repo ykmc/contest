823c5d09b414bcb63e03219ceac93a7996c4cbc1
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-08 20:11:28 +0900

    AC: atcoder/2014/1122_abc015/c.py

--- atcoder/2014/1122_abc015/c.py
+++ atcoder/2014/1122_abc015/c.py
@@ -10,3 +10,19 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+T = [list(map(int,input().split())) for _ in range(N)]
+
+from itertools import product
+prod = product(range(K),repeat=N)
+
+ans = "Nothing"
+for p in prod:
+    check = 0
+    for i in range(N):
+        check ^= T[i][p[i]]
+    if check==0:
+        ans = "Found"
+        break
+
+print(ans)
