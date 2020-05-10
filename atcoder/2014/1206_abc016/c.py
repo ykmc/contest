5ad27055977c5421214c0c2e3d5a71eafcf84e0b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-10 13:01:33 +0900

    AC: atcoder/2014/1206_abc016/c.py

--- atcoder/2014/1206_abc016/c.py
+++ atcoder/2014/1206_abc016/c.py
@@ -10,3 +10,20 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+AB = [tuple(map(int,input().split())) for _ in range(M)]
+
+A = [[float("inf") for _ in range(N)] for _ in range(N)]
+for a,b in AB:
+    A[a-1][b-1] = 1
+    A[b-1][a-1] = 1
+for i in range(N):
+    A[i][i] = 0
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            A[i][j] = min(A[i][j], A[i][k]+A[k][j])
+
+for i in range(N):
+    print(A[i].count(2))
