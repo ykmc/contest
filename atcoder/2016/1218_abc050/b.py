7d91502695e3630d330ee3eb9e62ac77c3f39e2b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-11 19:22:10 +0900

    AC: atcoder/2016/1218_abc050/b.py

--- atcoder/2016/1218_abc050/b.py
+++ atcoder/2016/1218_abc050/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+T = list(map(int,input().split()))
+M = int(input())
+PX = [list(map(int,input().split())) for _ in range(M)]
+ 
+total = sum(T)
+ 
+Ans = []
+for p,x in PX:
+    Ans.append(total - T[p-1] + x)
+ 
+for ans in Ans:
+    print(ans)
