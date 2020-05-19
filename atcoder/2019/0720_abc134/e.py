9e40fc95f13ee48f8e69a695043ec9e32627403f
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-19 21:45:46 +0900

    AC: atcoder/2019/0720_abc134/e.py

--- atcoder/2019/0720_abc134/e.py
+++ atcoder/2019/0720_abc134/e.py
@@ -10,3 +10,19 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+from collections import deque
+from bisect import bisect_left
+
+D = deque([])
+
+for a in A:
+    i = bisect_left(D,a)
+    if i==0:
+        D.appendleft(a)
+    else:
+        D[i-1] = a
+
+print(len(D))
