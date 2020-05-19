59e7a2254e8660f36a819934fe711f679326c852
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-19 21:45:29 +0900

    AC: atcoder/2019/0720_abc134/d.py

--- atcoder/2019/0720_abc134/d.py
+++ atcoder/2019/0720_abc134/d.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+
+B = [0]*(N+1)
+Ans = []
+
+for k in range(N,0,-1):
+    cnt = 0
+    for i in range(k,N+1,k):
+        cnt += B[i]
+    cnt %= 2
+    if A[k-1] != cnt:
+        B[k] = 1
+        Ans.append(k)
+
+print(len(Ans))
+if Ans:
+    print(*Ans)
