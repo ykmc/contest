46a808f0353d700102db04b4fe613760f4b83b41
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-20 20:59:55 +0900

    AC: atcoder/2015/0117_abc017/c.py

--- atcoder/2015/0117_abc017/c.py
+++ atcoder/2015/0117_abc017/c.py
@@ -10,3 +10,19 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+LRS = [tuple(map(int,input().split())) for _ in range(N)]
+
+# Sの合計
+total = 0
+# 累積和
+A = [0]*(M+1)
+for l,r,s in LRS:
+    A[l-1] += s
+    A[r] -= s
+    total += s
+
+for i in range(M):
+    A[i+1] += A[i]
+
+print(total - min(A[:M]))
