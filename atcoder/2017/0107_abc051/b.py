f3ebd34f7b5aaa25203fc2a28a2008fba0323154
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-20 21:06:34 +0900

    AC: atcoder/2017/0107_abc051/b.py

--- atcoder/2017/0107_abc051/b.py
+++ atcoder/2017/0107_abc051/b.py
@@ -10,3 +10,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+K,S = map(int,input().split())
+
+ans = 0
+for x in range(K+1):
+    for y in range(K+1):
+        if 0 <= S-(x+y) <= K:
+            ans += 1
+
+print(ans)
