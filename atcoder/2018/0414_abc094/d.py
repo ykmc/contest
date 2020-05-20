52194e27bf9657a8cd68c52f3d41f438b914bef9
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-20 21:45:38 +0900

    AC: atcoder/2018/0414_abc094/d.py

--- atcoder/2018/0414_abc094/d.py
+++ atcoder/2018/0414_abc094/d.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+
+# nCr : nが大きく、rがnの半分くらいの時が最大
+maxA = max(A)
+half = maxA/2
+
+t = 10**10
+for i in range(N):
+    # 最大の要素ではない and 真ん中からの絶対値が小さい → 値を更新
+    if A[i] != maxA and abs(A[i]-half) < abs(t-half):
+        t = A[i]
+
+print(maxA,t)
