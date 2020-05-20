e419b2606f4351439e820289185f4308a3dc6f36
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-20 21:08:09 +0900

    AC: atcoder/2017/0107_abc051/c.py

--- atcoder/2017/0107_abc051/c.py
+++ atcoder/2017/0107_abc051/c.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+Sx,Sy,Tx,Ty = map(int,input().split())
+
+dx = Tx - Sx
+dy = Ty - Sy
+ 
+ans1 = "U"*dy + "R"*dx
+ans2 = "D"*dy + "L"*dx
+ans3 = "L" + "U"*(dy+1) + "R"*(dx+1) + "D"
+ans4 = "R" + "D"*(dy+1) + "L"*(dx+1) + "U"
+ 
+print(ans1 + ans2 + ans3 + ans4)
