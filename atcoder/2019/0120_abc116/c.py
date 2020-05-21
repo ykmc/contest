dd78e92da120fca7eb4ea36c08b74e49303f1615
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-21 21:47:07 +0900

    AC: atcoder/2019/0120_abc116/c.py

--- atcoder/2019/0120_abc116/c.py
+++ atcoder/2019/0120_abc116/c.py
@@ -10,3 +10,25 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+H = list(map(int,input().split()))
+
+ans = 0
+x = [H]
+
+while len(x) > 0:
+    A = x.pop()
+    # 一番小さい値まで全域に水やり
+    minA = min(A)
+    ans += minA
+    k = A.index(minA)
+    # した結果の残り
+    for i in range(len(A)):
+        A[i] -= minA
+    # 一番小さい値 の左右に残りが存在するなら, 処理を継続する
+    if A[0:k] != []:
+        x.append(A[0:k])
+    if A[k+1:] != []:
+        x.append(A[k+1:])
+
+print(ans)
