431b42eb8c306a0dd7f26dc1544c7ff2042ab43c
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-23 18:47:13 +0900

    AC: atcoder/2019/0727_abc135/d.py

--- atcoder/2019/0727_abc135/d.py
+++ atcoder/2019/0727_abc135/d.py
@@ -1,4 +1,4 @@
-# Python3 (3.4.3)
+# PyPy3 (2.4.0)
 import sys
 input = sys.stdin.readline
 
@@ -10,3 +10,29 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+mod = 10**9+7
+
+# dp[i][j] : 上から i 桁までの数字で, 13で割ったあまりが j になる場合の数
+dp = [[0 for _ in range(13)] for _ in range(len(S))]
+
+# 初期化
+if S[0] == "?":
+    for i in range(10):
+        dp[0][i] += 1
+else:
+    dp[0][int(S[0])] = 1
+
+# dp更新
+for i in range(1,len(S)):
+    if S[i] == "?":
+        for j in range(13):
+            for k in range(10):
+                dp[i][(j*10 + k) % 13] += dp[i-1][j]
+                dp[i][(j*10 + k) % 13] %= mod
+    else:
+        for j in range(13):
+            dp[i][(j*10 + int(S[i])) % 13] += dp[i-1][j]
+            dp[i][(j*10 + int(S[i])) % 13] %= mod
+
+print(dp[-1][5])
