04b726057ab7f2a23ad45d3182e2a7d699bc1684
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-24 14:43:16 +0900

    AC: atcoder/2017/0115_abc052/c.py

--- atcoder/2017/0115_abc052/c.py
+++ atcoder/2017/0115_abc052/c.py
@@ -10,3 +10,38 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+mod = 10**9+7
+
+# 素数列挙
+primes = []
+is_prime = [True]*(N+1)
+for i in range(2,N+1):
+    if is_prime[i]:
+        primes.append(i)
+        j = 2
+        while i*j < N+1:
+            is_prime[i*j] = False
+            j += 1
+
+# 約数の集計用
+from collections import defaultdict
+div = defaultdict(int)
+
+ans = 1
+# N = 2 から全探索
+for i in range(2,N+1):
+    for p in primes:
+        if p > i:
+            break
+        cnt = 0
+        while i%p==0:
+            i //= p
+            cnt += 1
+        div[p] += cnt
+
+for d in div.values():
+    ans *= d+1
+    ans %= mod
+
+print(ans)
