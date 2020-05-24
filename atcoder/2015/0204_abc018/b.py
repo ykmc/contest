55564fcf6ef127b9fc05810a33138172e70bd385
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-24 14:17:45 +0900

    AC: atcoder/2015/0204_abc018/b.py

--- atcoder/2015/0204_abc018/b.py
+++ atcoder/2015/0204_abc018/b.py
@@ -10,3 +10,13 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+N = int(input())
+LR = [tuple(map(int,input().split())) for _ in range(N)]
+
+ans = list(S)
+for l,r in LR:
+    l,r = l-1,r-1
+    ans = ans[0:l] + ans[l:r+1][::-1] + ans[r+1:]
+
+print("".join(ans))
