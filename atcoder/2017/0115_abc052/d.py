ec30bd7d8b46dcd1336bc3047b5c4074c4e04830
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-24 14:43:26 +0900

    AC: atcoder/2017/0115_abc052/d.py

--- atcoder/2017/0115_abc052/d.py
+++ atcoder/2017/0115_abc052/d.py
@@ -10,3 +10,15 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,A,B = map(int,input().split())
+X = list(map(int,input().split()))
+
+ans = 0
+for i in range(N-1):
+    d = X[i+1] - X[i]
+    if d*A > B:
+        ans += B
+    else:
+        ans += d*A
+
+print(ans)
