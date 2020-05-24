e5f8abc73e5d4681c077ab38d78c4763b816d441
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-24 14:17:51 +0900

    AC: atcoder/2015/0204_abc018/c.py

--- atcoder/2015/0204_abc018/c.py
+++ atcoder/2015/0204_abc018/c.py
@@ -1,4 +1,4 @@
-# Python3 (3.4.3)
+# PyPy3 (2.4.0)
 import sys
 input = sys.stdin.readline
 
@@ -10,3 +10,40 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+R,C,K = map(int,input().split())
+S = [input().rstrip() for _ in range(R)]
+
+# UD[r][c][0] : (c,r)から見て, 上に存在するスペース数
+# UD[r][c][1] : (c,r)から見て, 下に存在するスペース数
+UD = [[[0,0] for _ in range(C)] for _ in range(R)]
+
+for c in range(C):
+    up,down = 0,0
+    for r in range(R):
+        # up
+        if S[r][c] == "x":
+            up = 0
+        else:
+            up += 1
+        UD[r][c][0] = up
+        # down
+        if S[R-1-r][C-1-c] == "x":
+            down = 0
+        else:
+            down += 1
+        UD[R-1-r][C-1-c][1] = down
+
+ans = 0
+# 各マスを中心として描けるか全探索
+for c in range(K-1,C-K+1):
+    for r in range(K-1,R-K+1):
+        flag = True
+        for k in range(K):
+            if min(min(UD[r][c-k]),min(UD[r][c+k])) < K-k:
+                flag = False
+                break
+        if flag:
+            ans += 1
+
+
+print(ans)
