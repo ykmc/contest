5c7e8bc58c752fd52b6bbaa5076852c5d3e1ab4a
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-26 21:28:53 +0900

    AC: atcoder/2018/1202_abc114/c.py

--- atcoder/2018/1202_abc114/c.py
+++ atcoder/2018/1202_abc114/c.py
@@ -6,7 +6,22 @@ input = sys.stdin.readline
 # function
 # -------------------------------------------------------------
 
+# s で始まる七五三数
+def dfs(s):
+    if int(s) > N:
+        return 0
+    if s.count("3") > 0 and s.count("5") > 0 and s.count("7") > 0:
+        cnt = 1
+    else:
+        cnt = 0
+    for c in "753":
+        cnt += dfs(s + c)
+    return cnt
+    
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+
+print(dfs("0"))
