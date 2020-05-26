7359c1ae92b0c0a22c00b1322a5ee5cbd4130858
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-26 20:17:12 +0900

    AC: atcoder/2019/0804_abc136/c.py

--- atcoder/2019/0804_abc136/c.py
+++ atcoder/2019/0804_abc136/c.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+H = list(map(int,input().split()))
+
+ans = "Yes"
+# 左端は1下げてもデメリットはないので下げておく
+H[0] -= 1
+for i in range(N-1):
+    if H[i] > H[i+1]:
+        ans = "No"
+        break
+    elif H[i] < H[i+1]:
+        H[i+1] -= 1
+
+print(ans)
