8fc819b367086c44b7249d0c9df040c22e30d282
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-26 20:50:51 +0900

    AC: atcoder/2018/0505_abc096/c.py

--- atcoder/2018/0505_abc096/c.py
+++ atcoder/2018/0505_abc096/c.py
@@ -10,3 +10,25 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+H,W = map(int,input().split())
+S = [input().rstrip() for _ in range(H)]
+
+for h in range(H):
+    for w in range(W):
+        # 白マスなら次へ
+        if S[h][w]==".":
+            continue
+        # 黒マスなら上下左右に黒マスが1つでもあれば ok, なければ ng
+        flg = False
+        for dh,dw in [(1,0), (-1,0), (0,1), (0,-1)]:
+            nh = h+dh
+            nw = w+dw
+            if 0 <= nh < H and 0 <= nw < W:
+                if S[nh][nw] == "#":
+                    flg = True
+                    break
+        if not flg:
+            print("No")
+            sys.exit()
+
+print("Yes")
