5f16ff9a5627b7e04ce7df9379c83bfe260d90a9
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 21:07:09 +0900

    AC: atcoder/2017/0211_abc054/b.py

--- atcoder/2017/0211_abc054/b.py
+++ atcoder/2017/0211_abc054/b.py
@@ -10,3 +10,20 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+A = [input().rstrip() for _ in range(N)]
+B = [input().rstrip() for _ in range(M)]
+
+for dh in range(N-M+1):
+    for dw in range(N-M+1):
+        flg = True
+        for h in range(M):
+            for w in range(M):
+                if B[h][w] != A[h+dh][w+dw]:
+                    flg = False
+                    break
+        if flg:
+            print("Yes")
+            sys.exit()
+
+print("No")
