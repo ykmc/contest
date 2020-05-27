f948fd08d8be89883e9163430f7db1c376dfbcc0
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 20:31:19 +0900

    AC: atcoder/2015/0411_abc021/d.py

--- atcoder/2015/0411_abc021/d.py
+++ atcoder/2015/0411_abc021/d.py
@@ -5,8 +5,40 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # function
 # -------------------------------------------------------------
+class Modinv:
+    def __init__(self, n, mod):
+        self.mod = mod
+        self.fact = [1]*n
+        self.finv = [1]*n
+        self.inv  = [1]*n
+        for i in range(2,n):
+            self.fact[i] = (self.fact[i-1]*i) % self.mod
+            self.inv[i]  = self.mod - self.inv[self.mod%i] * (self.mod//i)%self.mod
+            self.finv[i] = self.finv[i-1] * self.inv[i] % self.mod
 
+    def nPr(self, n, r):
+        if n<r:
+            return 0
+        else:
+            return self.fact[n] * self.finv[n-r] % self.mod
+
+    def nCr(self, n, r):
+        if n<r:
+            return 0
+        else:
+            return self.fact[n] * (self.finv[r] * self.finv[n-r] % self.mod) % self.mod
+
+    def nHr(self, n, r):
+        return self.nCr(n-1+r, n-1)
+
+mod = 10**9+7
+Modinv = Modinv(200010, mod)
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+K = int(input())
+
+print(Modinv.nHr(N,K))
+
