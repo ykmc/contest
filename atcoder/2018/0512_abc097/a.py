d6cfa439046f0ffa72a429b47427d3e9719a1fe6
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 21:28:03 +0900

    AC: atcoder/2018/0512_abc097/a.py

--- atcoder/2018/0512_abc097/a.py
+++ atcoder/2018/0512_abc097/a.py
@@ -10,3 +10,6 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+a,b,c,d = map(int,input().split())
+
+print("Yes" if abs(a-c)<=d or (abs(a-b)<=d and abs(b-c)<=d) else "No")
