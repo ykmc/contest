d14350d87eec8db5a75acc17671f16159187eedd
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 21:07:15 +0900

    AC: atcoder/2017/0211_abc054/c.py

--- atcoder/2017/0211_abc054/c.py
+++ atcoder/2017/0211_abc054/c.py
@@ -10,3 +10,30 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+AB = [tuple(map(int,input().split())) for _ in range(M)]
+
+A = [[False for _ in range(N)] for _ in range(N)]
+for a,b in AB:
+    A[a-1][b-1] = True
+    A[b-1][a-1] = True
+
+# 頂点を巡る順番を順列で生成する
+from itertools import permutations
+Ps = permutations([i for i in range(1,N)])
+
+ans = 0
+for P in Ps:
+    flg = True
+    x,y = 0,0
+    for p in P:
+        y = p
+        # 経路がなければ
+        if not A[x][y]:
+            flg = False
+            break
+        x = p
+    if flg:
+        ans += 1
+
+print(ans)
