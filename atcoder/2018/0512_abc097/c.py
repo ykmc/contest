071d983f90aa0ad0d65ce1c63a85f0a5088de489
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 21:28:14 +0900

    AC: atcoder/2018/0512_abc097/c.py

--- atcoder/2018/0512_abc097/c.py
+++ atcoder/2018/0512_abc097/c.py
@@ -10,3 +10,15 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+K = int(input())
+
+Ans = []
+for k in range(5):
+    for i in range(len(S)-k):
+        s = S[i:i+k+1]
+        if s not in Ans:
+            Ans.append(s)
+
+Ans.sort()
+print(Ans[K-1])
