5ebbce93cd0d74bf297d3f128c29b562cf84b5df
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 12:56:45 +0900

    AC: atcoder/2015/0411_abc021/b.py

--- atcoder/2015/0411_abc021/b.py
+++ atcoder/2015/0411_abc021/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A,B = map(int,input().split())
+K = int(input())
+P = list(map(int,input().split()))
+
+# start, goalが不明, 途中がPで寄り道なし
+#  => P + [start, goal] の各要素が1つずつなら ok
+
+P += [A,B]
+from collections import Counter
+C = Counter(P).most_common()
+
+print("YES" if C[0][1] == 1 else "NO")
