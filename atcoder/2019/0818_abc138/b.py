078ec978ed26dfff07625e8c77aa045e5c080ef5
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-28 07:42:47 +0900

    AC: atcoder/2019/0818_abc138/b.py

--- atcoder/2019/0818_abc138/b.py
+++ atcoder/2019/0818_abc138/b.py
@@ -10,3 +10,11 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+
+ans = 0
+for a in A:
+    ans += 1/a
+
+print(1/ans)
