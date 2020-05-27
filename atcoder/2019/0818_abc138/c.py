5f1046f0aa07f7ca760ab49b128c95e19ac4b610
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-28 07:45:24 +0900

    AC: atcoder/2019/0818_abc138/c.py

--- atcoder/2019/0818_abc138/c.py
+++ atcoder/2019/0818_abc138/c.py
@@ -10,3 +10,12 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+V = list(map(int,input().split()))
+
+V.sort()
+ans = V[0]
+for i in range(1,N):
+    ans = (ans + V[i]) / 2
+
+print(ans)
