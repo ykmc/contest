63df072ea2540d3475c165eb56ee3cd082adfdfc
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-28 07:36:31 +0900

    AC: atcoder/2018/1104_abc113/b.py

--- atcoder/2018/1104_abc113/b.py
+++ atcoder/2018/1104_abc113/b.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+T,A = map(int,input().split())
+H = list(map(int,input().split()))
+
+ans = -1
+ans_tp = 1000
+for i in range(N):
+    h = H[i]
+    tp = T - 0.006*h
+    if abs(ans_tp-A) > abs(tp-A):
+        ans_tp = tp
+        ans = i
+
+print(ans+1)
