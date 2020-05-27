3233f56118fe694c8e1729b087b7834f420dc616
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-27 12:46:57 +0900

    AC: atcoder/2019/0810_abc137/c.py

--- atcoder/2019/0810_abc137/c.py
+++ atcoder/2019/0810_abc137/c.py
@@ -10,3 +10,18 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+S = [list(input().rstrip()) for _ in range(N)]
+
+from collections import defaultdict
+d = defaultdict(int)
+
+for s in S:
+    s.sort()
+    d["".join(s)] += 1
+
+ans = 0
+for i in d.values():
+    ans += i*(i-1)//2
+
+print(ans)
