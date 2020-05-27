baaf7f7368f6c0ec1e69730c31b4f60f9e8dd5e9
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-28 07:36:37 +0900

    AC: atcoder/2018/1104_abc113/c.py

--- atcoder/2018/1104_abc113/c.py
+++ atcoder/2018/1104_abc113/c.py
@@ -10,3 +10,23 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+PY = [tuple(map(int,input().split())) for _ in range(M)]
+
+# 県ごとに分類
+A = [[] for _ in range(N+1)]
+for i in range(M):
+    p,y = PY[i]
+    A[p].append((y,i))
+
+for i in range(1,N+1):
+    A[i].sort()
+
+Ans = [0]*M
+for i in range(1,N+1):
+    for j in range(len(A[i])):
+        y,k = A[i][j]
+        Ans[k] = "{:06d}{:06d}".format(i,j+1)
+
+for ans in Ans:
+    print(ans)
