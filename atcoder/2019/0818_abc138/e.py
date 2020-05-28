da711114fa3a9dc3c3c3791b967806e5fce33515
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-28 14:12:29 +0900

    AC: atcoder/2019/0818_abc138/e.py

--- atcoder/2019/0818_abc138/e.py
+++ atcoder/2019/0818_abc138/e.py
@@ -10,3 +10,22 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+T = input().rstrip()
+
+# set:T が set:S に含まれない => S にない文字を T で使っていたら
+if not set(T) <= set(S):
+    print(-1)
+    sys.exit()
+
+ind = -1
+cnt = 0
+for t in T:
+    # 現在の index 以降から探索する
+    ind = S.find(t,ind+1)
+    # 見つからない場合は先頭から探索し直し, loop countを+1
+    if ind == -1:
+        cnt += 1
+        ind = S.find(t)
+
+print(len(S) * cnt + ind + 1)
