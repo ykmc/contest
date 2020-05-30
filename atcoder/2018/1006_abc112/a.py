3c514dd6d5581156aa790f39bcacb1cb46b744c8
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-30 13:58:37 +0900

    AC: atcoder/2018/1006_abc112/a.py

--- atcoder/2018/1006_abc112/a.py
+++ atcoder/2018/1006_abc112/a.py
@@ -10,3 +10,9 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+
+if N==1:
+    print("Hello World")
+else:
+    print(int(input()) + int(input()))
