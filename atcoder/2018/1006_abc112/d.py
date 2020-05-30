2d19e0a96fc7004f2d9003327bae63ac0a501f76
ykmc <34961813+ykmc@users.noreply.github.com>
2020-05-30 13:58:54 +0900

    AC: atcoder/2018/1006_abc112/d.py

--- atcoder/2018/1006_abc112/d.py
+++ atcoder/2018/1006_abc112/d.py
@@ -10,3 +10,22 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+
+ans = 0
+div1,div2 = [],[]
+for m in range(1,int(M**.5)+1):
+    if M%m == 0:
+        div1.append(m)
+
+for d in div1:
+    if M%d == 0:
+        div2.append(M//d)
+
+div3 = div1+div2
+div3.sort()
+for d in div3:
+    if d <= M//N:
+        ans = d
+
+print(ans)
