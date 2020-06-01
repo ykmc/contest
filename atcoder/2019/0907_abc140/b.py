8230c0d7e25503a098e9cfd23cfc8dcaa235ff4f
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-01 21:13:34 +0900

    AC: atcoder/2019/0907_abc140/b.py

--- atcoder/2019/0907_abc140/b.py
+++ atcoder/2019/0907_abc140/b.py
@@ -10,3 +10,19 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+C = list(map(int,input().split()))
+
+ans = 0
+for i in range(N):
+    # 食べた料理
+    k = A[i] - 1
+    # 得た満足度
+    ans += B[k]
+    # 追加満足度の判定
+    if i < N-1 and A[i] == A[i+1]-1:
+        ans += C[k]
+
+print(ans)
