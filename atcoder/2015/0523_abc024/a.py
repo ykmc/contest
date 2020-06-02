7821f7da611dda2807aeee737e4884248792ae26
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-02 21:27:28 +0900

    AC: atcoder/2015/0523_abc024/a.py

--- atcoder/2015/0523_abc024/a.py
+++ atcoder/2015/0523_abc024/a.py
@@ -10,3 +10,10 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A,B,C,K = map(int,input().split())
+S,T = map(int,input().split())
+
+ans = A*S + B*T
+if S+T >= K:
+    ans -= C*(S+T)
+print(ans)
