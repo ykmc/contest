27fc3cb52549fc5bf01d9b443ed414bd56e58b2f
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-03 19:52:41 +0900

    AC: atcoder/2015/0523_abc024/d.py

--- atcoder/2015/0523_abc024/d.py
+++ atcoder/2015/0523_abc024/d.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A = int(input())
+B = int(input())
+C = int(input())
+mod = 10**9+7
+ 
+AB = (A*B)%mod
+BC = (B*C)%mod
+CA = (C*A)%mod
+ 
+y = ((BC-AB)*pow((CA-BC+AB),mod-2,mod)) % mod
+x = ((BC-CA)*pow((AB-BC+CA),mod-2,mod)) % mod
+ 
+print(x,y)
