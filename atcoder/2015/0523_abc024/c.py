542644d439fb3cea6be6f0e0960e14b802fcce6f
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-03 19:52:35 +0900

    AC: atcoder/2015/0523_abc024/c.py

--- atcoder/2015/0523_abc024/c.py
+++ atcoder/2015/0523_abc024/c.py
@@ -10,3 +10,31 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,D,K = map(int,input().split())
+LR = [tuple(map(int,input().split())) for _ in range(D)]
+ST = [tuple(map(int,input().split())) for _ in range(K)]
+
+Ans = []
+
+for s,t in ST:
+    # 正の方向に進む場合
+    if s < t:
+        for i in range(D):
+            l,r = LR[i]
+            if l<=s<=r:
+                s = r
+            if s>=t:
+                Ans.append(i+1)
+                break
+    # 負の方向に進む場合
+    else:
+        for i in range(D):
+            l,r = LR[i]
+            if l<=s<=r:
+                s = l
+            if s<=t:
+                Ans.append(i+1)
+                break
+
+for ans in Ans:
+    print(ans)
