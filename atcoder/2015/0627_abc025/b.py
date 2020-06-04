b814ea8c584a98544ee21a145fb7f7163893479a
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-04 21:02:22 +0900

    AC: atcoder/2015/0627_abc025/b.py

--- atcoder/2015/0627_abc025/b.py
+++ atcoder/2015/0627_abc025/b.py
@@ -10,3 +10,28 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,A,B = map(int,input().split())
+SD = [tuple(input().split()) for _ in range(N)]
+
+pos = 0
+for s,d in SD:
+    d = int(d)
+    # 距離
+    if d <= A:
+        x = A
+    elif d >= B:
+        x = B
+    else:
+        x = d
+    # 方角
+    if s == "West":
+        x *= -1
+    
+    pos += x
+
+if pos == 0:
+    print(0)
+elif pos > 0:
+    print("East", pos)
+else:
+    print("West", -pos)
