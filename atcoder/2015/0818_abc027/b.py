de9a37f6826e43622dadca4621b658bd86d145e0
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-06 08:50:39 +0900

    AC: atcoder/2015/0818_abc027/b.py

--- atcoder/2015/0818_abc027/b.py
+++ atcoder/2015/0818_abc027/b.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+
+# N で割り切れなければ端数が出るので不可能
+if sum(A)%N != 0:
+    print(-1)
+else:
+    ans = 0
+    # ある島までの合計人数
+    total = 0
+    # 島あたりの目標人数
+    avg = sum(A)/N
+    for i in range(N):
+        total += A[i]
+        if total/(i+1) == avg:
+            continue
+        ans += 1
+    print(ans)
