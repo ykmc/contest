99eefb3513d617b689878f2308229e0131eea1f9
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-05 20:42:22 +0900

    AC: atcoder/2015/0711_abc026/b.py

--- atcoder/2015/0711_abc026/b.py
+++ atcoder/2015/0711_abc026/b.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+R = [int(input()) for _ in range(N)]
+
+R.sort(reverse=True)
+ans = 0
+for i in range(N):
+    if i%2 == 0:
+        ans += R[i]**2
+    else:
+        ans -= R[i]**2
+
+import math
+print(ans*math.pi)
