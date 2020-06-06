0fde2e8ec46906958abae6b462f51fb080792192
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-06 13:22:32 +0900

    AC: atcoder/2015/1024_abc030/a.py

--- atcoder/2015/1024_abc030/a.py
+++ atcoder/2015/1024_abc030/a.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+A,B,C,D = map(int,input().split())
+
+takahashi = B/A
+aoki = D/C
+
+if takahashi > aoki:
+    print("TAKAHASHI")
+elif aoki > takahashi:
+    print("AOKI")
+else:
+    print("DRAW")
