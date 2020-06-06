206d94da2c719de7987bf53c54efb882eda78ab5
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-06 20:00:20 +0900

    AC: atcoder/2015/1121_abc031/b.py

--- atcoder/2015/1121_abc031/b.py
+++ atcoder/2015/1121_abc031/b.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+L,H = map(int,input().split())
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+for a in A:
+    if L <= a <= H:
+        print(0)
+    elif a < L:
+        print(L-a)
+    else:
+        print(-1)
