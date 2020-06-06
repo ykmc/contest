f7c2ac8f9197210c74b619d49cfb018a7541a678
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-06 14:17:46 +0900

    AC: atcoder/2017/0326_abc057/b.py

--- atcoder/2017/0326_abc057/b.py
+++ atcoder/2017/0326_abc057/b.py
@@ -10,3 +10,21 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,M = map(int,input().split())
+AB = [tuple(map(int,input().split())) for _ in range(N)]
+CD = [tuple(map(int,input().split())) for _ in range(M)]
+
+Ans = [-1]*N
+for i in range(N):
+    a,b = AB[i]
+    md0 = 10**10
+
+    for j in range(M):
+        c,d = CD[j]
+        md = abs(a-c) + abs(b-d)
+        if md < md0:
+            md0 = md
+            Ans[i] = (j+1)
+
+for ans in Ans:
+    print(ans)
