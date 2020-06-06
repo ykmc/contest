60bd49e775dfc7fa4a3262ffc4f7f20432a1cb66
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-06 14:17:52 +0900

    AC: atcoder/2017/0326_abc057/c.py

--- atcoder/2017/0326_abc057/c.py
+++ atcoder/2017/0326_abc057/c.py
@@ -5,8 +5,26 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # function
 # -------------------------------------------------------------
-
+def F(a,b):
+    return max(len(str(a)), len(str(b)))
 
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+
+ds = set()
+dl = list()
+for i in range(1,10**5+1):
+    if N%i == 0:
+        ds.add(i)
+        dl.append(i)
+for d in dl:
+    ds.add(N//d)
+D = sorted(list(ds))
+
+ans = 100
+for d in D:
+    ans = min(ans,F(d,N//d))
+
+print(ans)
