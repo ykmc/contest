6d760cc48ade7950dc81c28db9b1bab3dfe5f9cd
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-08 21:20:08 +0900

    AC: atcoder/2016/0206_abc033/b.py

--- atcoder/2016/0206_abc033/b.py
+++ atcoder/2016/0206_abc033/b.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+SP = []
+for _ in range(N):
+    s,p = input().split()
+    SP.append((s,int(p)))
+
+SP.sort(key=lambda x:x[1], reverse=True)
+
+maxP = SP[0][1]
+totalP = 0
+for s,p in SP:
+    totalP += p
+
+print(SP[0][0] if maxP > totalP/2 else "atcoder")
