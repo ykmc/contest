229858e8b4e4b3d10fd4575c116e712942a18bb2
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-09 19:59:45 +0900

    AC: atcoder/2016/0326_abc035/b.py

--- atcoder/2016/0326_abc035/b.py
+++ atcoder/2016/0326_abc035/b.py
@@ -10,3 +10,32 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+S = input().rstrip()
+t = int(input())
+
+x,y,z = 0,0,0
+for s in S:
+    if s == "R":
+        x += 1
+    elif s == "L":
+        x -=  1
+    elif s == "U":
+        y += 1
+    elif s == "D":
+        y -= 1
+    else:
+        z += 1
+
+# max
+if t == 1:
+    print(abs(x) + abs(y) + z)
+# min
+else:
+    d = abs(x) + abs(y) - z
+    if d > 0:
+        print(d)
+    else:
+        if d%2 == 0:
+            print(0)
+        else:
+            print(1)
