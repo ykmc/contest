c6de0987c108e3f4fec027913a8415bdcfbcd5b8
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-09 20:16:13 +0900

    AC: atcoder/2016/0507_abc037/b.py

--- atcoder/2016/0507_abc037/b.py
+++ atcoder/2016/0507_abc037/b.py
@@ -10,3 +10,14 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,Q = map(int,input().split())
+LRT = [tuple(map(int,input().split())) for _ in range(Q)]
+
+Ans = [0]*N
+
+for l,r,t in LRT:
+    for i in range(l-1,r):
+        Ans[i] = t
+
+for ans in Ans:
+    print(ans)
