c547726be8d85ff9c323e44e4fd3e8dcaf1815c0
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-09 20:16:22 +0900

    AC: atcoder/2016/0507_abc037/c.py

--- atcoder/2016/0507_abc037/c.py
+++ atcoder/2016/0507_abc037/c.py
@@ -10,3 +10,17 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+
+K = min(K,N-K+1)
+ans = 0
+for i in range(N):
+    if i < K:
+        ans += A[i]*(i+1)
+    elif i < N-K:
+        ans += A[i]*K
+    else:
+        ans += A[i]*(N-i)
+
+print(ans)
