e2b4b4a7d3e208893f92758cbfcf13c765779e6b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-09 19:59:52 +0900

    AC: atcoder/2016/0326_abc035/c.py

--- atcoder/2016/0326_abc035/c.py
+++ atcoder/2016/0326_abc035/c.py
@@ -10,3 +10,16 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+n,q = map(int,input().split())
+LR = [tuple(map(int,input().split())) for _ in range(q)]
+
+A = [0]*(n+1)
+for l,r in LR:
+    A[l-1] += 1
+    A[r] -= 1
+
+for i in range(n):
+    A[i] %= 2
+    A[i+1] += A[i]
+
+print("".join(map(str,A[:n])))
