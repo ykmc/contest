2fc6ecdf0fecad6d62ed9e71f166213786d15d2b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-15 17:48:43 +0900

    AC: atcoder/2016/0528_abc038/b.py

--- atcoder/2016/0528_abc038/b.py
+++ atcoder/2016/0528_abc038/b.py
@@ -10,3 +10,7 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+H1,W1 = map(int,input().split())
+H2,W2 = map(int,input().split())
+
+print("YES" if H1 in (H2,W2) or W1 in (H2,W2) else "NO")
