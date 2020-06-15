b246959279ac15e7e52912b7c20c0a8122c5c98b
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-15 17:48:48 +0900

    AC: atcoder/2016/0528_abc038/c.py

--- atcoder/2016/0528_abc038/c.py
+++ atcoder/2016/0528_abc038/c.py
@@ -10,3 +10,18 @@ input = sys.stdin.readline
 # -------------------------------------------------------------
 # main
 # -------------------------------------------------------------
+N = int(input())
+A = list(map(int,input().split()))
+
+ans = 0
+cnt = 0
+prev = 0
+for a in A:
+    if prev < a:
+        cnt += 1
+    else:
+        cnt = 1
+    ans += cnt
+    prev = a
+
+print(ans)
