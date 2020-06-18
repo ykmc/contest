bb54656433257a135dff33e0edf78efb5da47ed6
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-18 20:28:46 +0900

    AC: atcoder/2016/0618_abc040/c.py

--- atcoder/2016/0618_abc040/c.py
+++ atcoder/2016/0618_abc040/c.py
@@ -2,3 +2,14 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+A = list(map(int,input().split()))
+
+dp = [0]*N
+dp[0] = 0
+dp[1] = abs(A[1]-A[0])
+
+for i in range(2,N):
+    dp[i] = min(dp[i-1] + abs(A[i-1]-A[i]), dp[i-2] + abs(A[i-2]-A[i]))
+
+print(dp[-1])
