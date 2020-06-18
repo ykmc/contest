8f67a7b471664f5ce9c3e77e770d7803bcb244b6
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-18 20:28:41 +0900

    AC: atcoder/2016/0618_abc040/b.py

--- atcoder/2016/0618_abc040/b.py
+++ atcoder/2016/0618_abc040/b.py
@@ -1,3 +1,12 @@
 # Python3 (3.8.2)
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+
+ans = 10**10
+for x in range(1,int(N**.5)+1):
+    y = N//x
+    ans = min(ans, abs(x-y) + N%x)
+
+print(ans)
