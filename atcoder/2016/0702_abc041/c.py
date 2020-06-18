d1cca466bfc8469ef325c0ced6e2cc226cb5aa5d
ykmc <34961813+ykmc@users.noreply.github.com>
2020-06-18 20:35:20 +0900

    AC: atcoder/2016/0702_abc041/c.py

--- atcoder/2016/0702_abc041/c.py
+++ atcoder/2016/0702_abc041/c.py
@@ -1,3 +1,14 @@
 # Python3 (3.8.2)
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+
+D = {}
+for i in range(N):
+    D[i+1] = A[i]
+
+D = sorted(D.items(),key=lambda x: x[1], reverse=True)
+for i in range(N):
+    print(D[i][0])
